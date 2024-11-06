f9b90a366e95619d034678de4c69d1c2b2f39982
Vu Huynh Thanh <vu.huynh@titancorpvn.com>
2024-11-06 09:31:30 +0700

    Update video 61

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -1,9 +1,14 @@
 package vn.hoidanit.laptopshop.controller;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class UserController {
@@ -20,6 +25,18 @@ public class UserController {
         String test = this.userService.handleHello();
         model.addAttribute("vu", test);
         model.addAttribute("hoidanit", "from controller with model");
+        return "hello";
+    }
+
+    @GetMapping("/admin/user")
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
+        return "/admin/user/create";
+    }
+
+    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hodanit) {
+        System.out.println("run here" + hodanit);
         return "hello";
     }
 }
