1d0d7387fd65324e3519aa4529f706ae6fa2631f
Vu Huynh Thanh <vu.huynh@titancorpvn.com>
2024-11-07 11:02:19 +0700

    Update video 67

--- src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package vn.hoidanit.laptopshop.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<User> findOneByEmail(String email);
 
     List<User> findAll();
+
+    Optional<User> findById(long id);
 }
