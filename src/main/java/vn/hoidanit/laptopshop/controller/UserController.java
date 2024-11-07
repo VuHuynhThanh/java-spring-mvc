cc19a38f2774f46ddeb0958c4a45320215c9b5a6
Vu Huynh Thanh <vu.huynh@titancorpvn.com>
2024-11-07 09:41:19 +0700

    Update video 71

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -32,15 +32,22 @@ public class UserController {
 
     @GetMapping("/admin/user")
     public String getUserPage(Model model) {
+        List<User> users = this.userService.getAllUsers();
+        model.addAttribute("users1", users);
+        return "/admin/user/table-user";
+    }
+
+    @RequestMapping(value = "/admin/user/create") // Defaul method: GET
+    public String getCreateUserPage(Model model) {
         model.addAttribute("newUser", new User());
         return "/admin/user/create";
     }
 
-    @RequestMapping(value = "/admin/user/create1", method = RequestMethod.POST)
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
     public String createUserPage(Model model, @ModelAttribute("newUser") User hodanit) {
         System.out.println("run here " + hodanit);
         this.userService.handleSaveUser(hodanit);
-        return "hello";
+        return "redirect:/admin/user";
     }
 }
 
