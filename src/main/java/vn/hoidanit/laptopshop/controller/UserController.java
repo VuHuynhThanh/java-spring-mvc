9999d8030bd1d37cb12c48a86f96615eff6b45e6
Vu Huynh Thanh <vu.huynh@titancorpvn.com>
2024-11-08 15:39:08 +0700

    Update video 77

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -77,6 +77,8 @@ public class UserController {
             currentUser.setPhone(hodanit.getFullName());
 
             this.userService.handleSaveUser(currentUser);
+
+            int dd = 120;
         }
         return "redirect:/admin/user";
     }
--- src/main/java/vn/hoidanit/laptopshop/domain/User.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/User.java
@@ -15,6 +15,7 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
 
     public long getId() {
         return id;
@@ -64,9 +65,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", email=" + email + ", passWord=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+        return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
+
 }
